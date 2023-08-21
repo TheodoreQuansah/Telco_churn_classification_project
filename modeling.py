9c11af24e426a925b6fa7620603d4a7bbac60b08
TheodoreQuansah <theodore.s.quansah@gmail.com>
2023-08-21 00:43:08 -0500

    4th edit

--- modeling.py
+++ modeling.py
@@ -1,92 +1,10 @@
-import os
-import numpy as np
 import pandas as pd
-import scipy.stats as stats
-import matplotlib.pyplot as plt
 
-from scipy import stats
-from env import get_connection
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
-
-#################################### cleaning data columns #######################################################################
-
-def encode_columns(train, val, test):
-    # One-hot encoding categorical columns with get dummies
-    train = pd.get_dummies(train, columns=[
-        'gender', 'senior_citizen', 'partner', 'dependents', 'phone_service',
-        'multiple_lines', 'online_security', 'online_backup', 'device_protection',
-        'tech_support', 'streaming_tv', 'streaming_movies', 'payment_type',
-        'contract_type', 'internet_service_type', 'paperless_billing'
-    ], drop_first=True)
-    
-    val = pd.get_dummies(val, columns=[
-        'gender', 'senior_citizen', 'partner', 'dependents', 'phone_service',
-        'multiple_lines', 'online_security', 'online_backup', 'device_protection',
-        'tech_support', 'streaming_tv', 'streaming_movies', 'payment_type',
-        'contract_type', 'internet_service_type', 'paperless_billing'
-    ], drop_first=True)
-    
-    test = pd.get_dummies(test, columns=[
-        'gender', 'senior_citizen', 'partner', 'dependents', 'phone_service',
-        'multiple_lines', 'online_security', 'online_backup', 'device_protection',
-        'tech_support', 'streaming_tv', 'streaming_movies', 'payment_type',
-        'contract_type', 'internet_service_type', 'paperless_billing'
-    ], drop_first=True)
-
-    # Drop columns with 'No internet service' and 'internet_service_type_None'
-    cols_to_drop = train.columns[train.columns.str.contains('No internet service')]
-    train = train.drop(columns=cols_to_drop)
-    train = train.drop(columns=['internet_service_type_None'])
-    train = train.rename(columns={'gender_male': 'gender'})
-
-    cols_to_drop = val.columns[val.columns.str.contains('No internet service')]
-    val = val.drop(columns=cols_to_drop)
-    val = val.drop(columns=['internet_service_type_None'])
-    val = val.rename(columns={'gender_male': 'gender'})
-
-    cols_to_drop = test.columns[test.columns.str.contains('No internet service')]
-    test = test.drop(columns=cols_to_drop)
-    test = test.drop(columns=['internet_service_type_None'])
-    test = test.rename(columns={'gender_male': 'gender'})
-
-    # Rename columns by removing "_Yes"
-    columns_to_rename = [
-        "partner_Yes", "dependents_Yes", "phone_service_Yes", 
-        "multiple_lines_Yes", "online_security_Yes", "online_backup_Yes", 
-        "device_protection_Yes", "tech_support_Yes", "streaming_tv_Yes", 
-        "streaming_movies_Yes", "paperless_billing_Yes"
-    ]
-    for col in columns_to_rename:
-        new_name = col.replace("_Yes", "")
-        train.rename(columns={col: new_name}, inplace=True)
-        val.rename(columns={col: new_name}, inplace=True)
-        test.rename(columns={col: new_name}, inplace=True)
-
-    # Rename columns based on provided replacements
-    columns_to_rename = [
-        "senior_citizen_1", "payment_type_Manual Payment", 
-        "contract_type_one/two-years", "internet_service_type_Fiber optic"
-    ]
-    for col in columns_to_rename:
-        new_name = col.replace("senior_citizen_1", "senior_citizen").replace("payment_type_Manual Payment",
-                                                                             "payment_type").replace("contract_type_one/two-years", 
-                                                                             "contract_type").replace("internet_service_type_Fiber optic", 
-                                                                             "internet_service_type")
-        train.rename(columns={col: new_name}, inplace=True)
-        val.rename(columns={col: new_name}, inplace=True)
-        test.rename(columns={col: new_name}, inplace=True)
-    
-    return train, val, test
-
-
-
-
-
+from sklearn.preprocessing import MinMaxScaler
 
 
 #################################### creating x, y subsets #######################################################################
@@ -101,22 +19,26 @@ def x_y_split(train, val, test):
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 
-
-
-
-
 #################################### decision tree #######################################################################
 
 def train_decision_tree(X_train, y_train, X_val, y_val, max_depth=3, random_state=42):
+    # Exclude non-numeric columns from the training and validation sets
+    numeric_columns = X_train.select_dtypes(include=['number']).columns.tolist()
+    X_train = X_train[numeric_columns]
+    X_val = X_val[numeric_columns]
+
     # Create a Decision Tree model
     clf = DecisionTreeClassifier(max_depth=max_depth, random_state=random_state)
+    
     # Fit the model on the training data
     clf.fit(X_train, y_train)
     
     # Calculate feature importances
     fi = pd.DataFrame({'Feature': X_train.columns, 'Importance': clf.feature_importances_})
-    fi = fi.sort_values(by='Importance', ascending=False).head(3)
-    
+
+    # Get the top 3 important features
+    mfi = fi.sort_values(by='Importance', ascending=False).head(3)
+
     # Calculate accuracy on training and validation data
     train_accuracy = clf.score(X_train, y_train)
     val_accuracy = clf.score(X_val, y_val)
@@ -124,15 +46,21 @@ def train_decision_tree(X_train, y_train, X_val, y_val, max_depth=3, random_stat
     # Print accuracy statements
     print(f"Accuracy of Decision Tree on train data is {round(train_accuracy, 4)}")
     print(f"Accuracy of Decision Tree on validate data is {round(val_accuracy, 4)}")
-    
-    # Return only the feature importance DataFrame
-    return fi
+
+    # Return the trained model
+    return mfi
+
 
 
 
 #################################### random forest #######################################################################
 
 def train_random_forest(X_train, y_train, X_val, y_val):
+    # Exclude non-numeric columns from the training and validation sets
+    numeric_columns = X_train.select_dtypes(include=['number']).columns.tolist()
+    X_train = X_train[numeric_columns]
+    X_val = X_val[numeric_columns]
+    
     # Create a Random Forest model
     rf = RandomForestClassifier(min_samples_leaf = 7, max_depth=8, random_state=42)
     # Fit the model on the training data
@@ -152,7 +80,6 @@ def train_random_forest(X_train, y_train, X_val, y_val):
     
     # Return only the feature importance DataFrame
     return fi
-
 
 
 #################################### KNeighbors #######################################################################
@@ -187,18 +114,14 @@ def train_knn(train, val, n_neighbors=30):
     print(f"Accuracy of KNN on validate data is {round(val_accuracy, 4)}")
 
 
-
-
 #################################### LogisticRegression #######################################################################
 
-def train_logistic_regression(train, val):
-    # Separate features and target
-    X_train = train.drop(columns=['churn'])
-    y_train = train['churn']
-
-    X_val = val.drop(columns=['churn'])
-    y_val = val['churn']
-
+def train_logistic_regression(X_train, y_train, X_val, y_val):
+    # Exclude non-numeric columns from the training and validation sets
+    numeric_columns = X_train.select_dtypes(include=['number']).columns.tolist()
+    X_train = X_train[numeric_columns].copy()
+    X_val = X_val[numeric_columns].copy()
+    
     # Apply Min-Max scaling to the selected columns
     mms = MinMaxScaler()
     X_train[['tenure', 'monthly_charges', 'total_charges']] = mms.fit_transform(X_train[['tenure', 'monthly_charges', 'total_charges']])
@@ -217,7 +140,6 @@ def train_logistic_regression(train, val):
     # Print accuracy statements
     print(f"Accuracy of Logistic Regression on train data is {round(train_accuracy, 4)}")
     print(f"Accuracy of Logistic Regression on validate data is {round(val_accuracy, 4)}")
-
 
 
 
@@ -245,9 +167,58 @@ def test_knn(train, test, n_neighbors=30):
     # Fit the model on the training data
     knn.fit(X_train, y_train)
     
-    # Calculate accuracy on training and validation data
+    # Calculate accuracy on training and test data
     train_accuracy = knn.score(X_train, y_train)
     test_accuracy = knn.score(X_test, y_test)
+   
     
-    # Print accuracy statements
+    # Print accuracy statement
     print(f"Accuracy of KNN on test data is {round(test_accuracy, 4)}")
+
+
+#################################### knn_predictions #######################################################################
+
+def generate_knn_predictions(train, test, n_neighbors, output_csv_filename):
+    # Select the specified columns
+    columns = ['customer_id', 'tenure', 'monthly_charges', 'total_charges', 'contract_type', 'internet_service_type']
+    X_train = train[columns].copy()
+    X_train.drop(columns=['customer_id'], inplace=True)
+    y_train = train['churn']
+
+    X_test = test[columns].copy()
+    X_test.drop(columns=['customer_id'], inplace=True)
+
+    # Apply Min-Max scaling to the selected columns
+    mms = MinMaxScaler()
+    X_train[['tenure', 'monthly_charges', 'total_charges']] = mms.fit_transform(X_train[['tenure', 'monthly_charges', 'total_charges']])
+    X_test[['tenure', 'monthly_charges', 'total_charges']] = mms.transform(X_test[['tenure', 'monthly_charges', 'total_charges']])
+
+    # Create a KNN model
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+
+    # Fit the model on the training data
+    knn.fit(X_train, y_train)
+
+    # Predict probabilities and labels on the test set
+    y_proba = knn.predict_proba(X_test)[:, 1]  # Probability of churn
+    y_pred = knn.predict(X_test)  # Predicted labels
+
+    # Assuming you have the customer_id column in your X_test DataFrame
+    customer_ids = test['customer_id'].reset_index(drop=True)
+
+    # Create a DataFrame for predictions
+    predictions_df = pd.DataFrame({
+        'customer_id': customer_ids,
+        'probability_of_churn': y_proba,
+        'prediction_of_churn': y_pred
+    })
+
+    # Map the binary predictions (1=churn, 0=not_churn)
+    predictions_df['prediction_of_churn'] = predictions_df['prediction_of_churn'].astype(int)
+
+    # Save the predictions to a CSV file
+    predictions_df.to_csv(output_csv_filename, index=False)
+
+
+
+
